d534499a5030ce633536f27200143d738e5bea98
Rahul Rajagopalan <rahulrajago@gmail.com>
2012-04-07 14:59:20 -0400

    Fixed a syntax error in the config file.

--- scripts/jqm-config.js
+++ scripts/jqm-config.js
@@ -13,8 +13,8 @@ $(document).on('mobileinit', function() {
   $.mobile.linkBindingEnabled = false;
   $.mobile.hashListeningEnabled = false;
   $.mobile.pushStateEnabled = false;
-}
+});
 
 $(document).on('pagehide', 'div[data-role="page"]', function(event, ui) {
   $(event.currentTarget).remove();
-}
+});
