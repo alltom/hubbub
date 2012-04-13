96e8d8933728bba098d8a15002b4b116f2599f41
Rahul Rajagopalan <rahulrajago@gmail.com>
2012-04-12 22:51:07 -0400

    Added some comments to router.js to make it easier to understand.

--- scripts/router.js
+++ scripts/router.js
@@ -4,6 +4,15 @@
  */
 hubbub.Router = Backbone.Router.extend({
 
+  /* 
+   * Routes for the application.
+   * e.g. if we have an anchor with href="#filter", then the URL will change
+   * to .../index.html#filter
+   * On URL change, Backbone.js checks this object and fires the function
+   * associated with the route (also named filter in this case)
+   *
+   * The first page to load (the '' route) is the feed item list.
+   */
   routes: {
     '': 'listFeedItems',
     'feed-items': 'listFeedItems',
@@ -12,17 +21,21 @@ hubbub.Router = Backbone.Router.extend({
 
   /**
    * Initialize the router. Start off with some stub feed items.
+   * This happens when the application starts.
    */
   initialize: function() {
     this.feedItems = hubbub.stubFeedItems();    
     this.firstPage = true;
 
-    // Load all templates, since changePage gets rid of them
+    // Eagerly load all templates, since changePage gets rid of them
     this.feedPageTemplate = $('#feedPageTemplate');
     this.feedItemTemplate = $('#feedItemTemplate');
     this.filterTemplate = $('#filterTemplate');
   },
 
+  /**
+   * Show the news feed.
+   */
   listFeedItems: function() {
     this.changePage(new hubbub.FeedPageView({
       model: this.feedItems,
@@ -31,6 +44,9 @@ hubbub.Router = Backbone.Router.extend({
     }));
   },
 
+  /**
+   * Show the filter page.
+   */
   filter: function() {
     this.changePage(new hubbub.FilterView({
       filterTemplate: this.filterTemplate
@@ -38,21 +54,25 @@ hubbub.Router = Backbone.Router.extend({
   },
 
   /**
-   * Change the page displayed.
-   * Given a View, calls render() on that view, then appends that view's
-   * element to the <body> element.
+   * Helper method to change the page displayed.
+   * Given a View, 
+   * 1. calls render() on that View
+   * 2. appends that view's element to the <body> element
+   * 3. calls $.mobile.changePage, passing in the new View's element, which
+   *    removes the HTML for the previous page, and shows an animation on
+   *    all page changes except the very first one.
    */
-  changePage: function(page) {
-    $(page.el).attr('data-role', 'page');
-    page.render();
-    $('body').append($(page.el));
+  changePage: function(pageView) {
+    $(pageView.el).attr('data-role', 'page');
+    pageView.render();
+    $('body').append($(pageView.el));
     var transition = $.mobile.defaultPageTransition;
     // Don't transition for the first page
     if (this.firstPage) {
       transition = 'none';
       this.firstPage = false;
     }
-    $.mobile.changePage($(page.el), {
+    $.mobile.changePage($(pageView.el), {
       changeHash: false,
       transition: transition
     });
