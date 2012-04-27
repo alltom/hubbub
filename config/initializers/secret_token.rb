7f1f4083633da4dee457cc2aa660a9d1bbfdace6
Tom Lieber <tom@alltom.com>
2012-04-27 09:21:05 -0400

    turned into a rails project, integrated Javascripts and stylesheets into the asset pipeline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubbub::Application.config.secret_token = '20dde8311ad1c32ba56ac0f6618d8b386660b8d740b98fbfa4a93ac312827a75964b291ba0da91e33cb2edf53cd5c03067f4ecb5ae999f3e769ac6af9622b791'
