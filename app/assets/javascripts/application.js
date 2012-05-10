60a205550865089d581fcf2b84e878712034235f
Tom Lieber <tom@alltom.com>
2012-05-10 00:38:40 -0500

    load the Javascripts which compose the main site in home.js instead of application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree ./lib
-//= require hubbub.js
-//= require_tree ./models
-//= require_tree ./views
-//= require stubs.js
-//= require router.js
-//= require templates.js.coffee
-//= require main.js
---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,7 @@
+//= require hubbub.js
+//= require_tree ./models
+//= require_tree ./views
+//= require stubs.js
+//= require router.js
+//= require templates.js.coffee
+//= require main.js
