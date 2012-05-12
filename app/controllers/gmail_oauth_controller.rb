97643460da9f9e5d49955aa5767234bc987fcd56
Rahul Rajagopalan <rahulrajago@gmail.com>
2012-05-11 23:06:55 -0400

    Extracted class on the Gmail oauth stuff too.
    Because to find things in a huge class I have to scroll, but to find
    files in a huge project, I just hit Ctrl-P and my editor reads my mind :D

---
+++ app/controllers/gmail_oauth_controller.rb
@@ -0,0 +1,82 @@
+class GmailOauthController < ApplicationController
+  # Just render the form asking for the user's gmail address.
+  def ask_for_gmail
+  end
+
+  # Can't use Omniauth, so this is a bit more manual..
+  # Adapted from:
+  # https://github.com/nfo/gmail-oauth-sinatra/blob/master/app.rb
+  # Basically, you create an OAuth::Consumer, use it to get a request token,
+  # then use the request token to get an access token, which is what the
+  # Gmail gem needs.
+  def gmail_setup
+    # Hey, is there an idiomatic way to do validations without a model?
+    # I'm just doing it manually here.
+    email = params[:email] 
+    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    if email_regex.match(email).nil? # Failed
+      flash[:alert] = "This doesn't seem to be an email."
+      redirect_to '/auth/gmail/form' and return
+    end
+    split_on_at = email.split '@'
+    if split_on_at[1] != 'gmail.com'
+      flash[:alert] = "This isn't a gmail.com email address."
+      redirect_to '/auth/gmail/form' and return
+    end
+
+    session[:gmail_address] = email
+
+    consumer_key = Rails.configuration.google_consumer_key
+    consumer_secret = Rails.configuration.google_consumer_secret
+    
+    consumer = OAuth::Consumer.new(consumer_key, consumer_secret,
+      :site => "https://www.google.com",
+      :request_token_path => '/accounts/OAuthGetRequestToken?scope=https://mail.google.com/%20https://www.googleapis.com/auth/userinfo%23email',
+      :access_token_path => '/accounts/OAuthGetAccessToken',
+      :authorize_path => '/accounts/OAuthAuthorizeToken'
+    )
+
+    request_token = consumer.get_request_token(
+        :oauth_callback => 'http://localhost:3000/auth/gmail/callback')
+    session[:gmail_request_token] = request_token.token
+    session[:gmail_request_secret] = request_token.secret
+
+    redirect_to request_token.authorize_url
+  end
+
+  def refresh_gmail
+    access = GmailAccess.create(
+        email = session[:gmail_address],
+        oauth_token = session[:gmail_token],
+        oauth_token_secret = session[:gmail_secret]
+    )
+    access.emails
+  end
+
+  def google_callback
+    # TODO Extract shared code to a before_filter
+    consumer_key = Rails.configuration.google_consumer_key
+    consumer_secret = Rails.configuration.google_consumer_secret
+    
+    consumer = OAuth::Consumer.new(consumer_key, consumer_secret,
+      :site => "https://www.google.com",
+      :request_token_path => '/accounts/OAuthGetRequestToken?scope=https://mail.google.com/%20https://www.googleapis.com/auth/userinfo%23email',
+      :access_token_path => '/accounts/OAuthGetAccessToken',
+      :authorize_path => '/accounts/OAuthAuthorizeToken'
+    )
+
+    request_token = OAuth::RequestToken.new(consumer,
+        session[:gmail_request_token], session[:gmail_request_secret])
+
+    access_token = request_token.get_access_token(
+        :oauth_verifier => params[:oauth_verifier])
+
+    # These are the values we need in GmailAccess
+    session[:gmail_token] = access_token.token
+    session[:gmail_secret] = access_token.secret
+
+    refresh_gmail
+
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -24,9 +24,9 @@ Hubbub::Application.routes.draw do
   #    gmail address (we need it) The form posts to /auth/gmail.
   # 2. Validate, then send them to Google, they ask them to authenticate us.
   # 3. Google calls auth/gmail/callback which logs OAuth tokens. 
-  match 'auth/gmail/form', :to => 'sessions#ask_for_gmail'
-  match 'auth/gmail', :to => 'sessions#gmail_setup'
-  match 'auth/gmail/callback', :to => 'sessions#google_callback'
+  match 'auth/gmail/form', :to => 'gmail_oauth#ask_for_gmail'
+  match 'auth/gmail', :to => 'gmail_oauth#gmail_setup'
+  match 'auth/gmail/callback', :to => 'gmail_oauth#google_callback'
 
   # Imgur doesn't actually require authentication, but we can fake it for now.
   # Through this, the user is explicitly saying that they want to see Imgur
