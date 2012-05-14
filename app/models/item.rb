aed5934c479dbe7b100972d5d71f7f60ed184ffa
Tom Lieber <tom@alltom.com>
2012-05-14 01:51:56 -0400

    tweaked item fill algorithm to work even when only one service is connected

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,18 +2,19 @@ class Item
   def self.recent user, limit=12
     # Grabbing the limit most recent items regardless of table tends to produce
     # an unbalanced feed of mostly Twitter posts and a little Facebook.
-    # Instead, grab a minimum from each source, because we want to demonstrate
-    # that we have live data from all four services.
-    #
-    # This also fixes the issue where we use up extra time by querying the
-    # database for limit * 4 items, then throw out limit * 3.
+    # Instead, grab items from each source round robin style so that every service
+    # has an equal number of items in the common case.
     # 
     # Sorting tends to bunch up services. Do you think this is a problem?
-    from_each = limit / 4
-    items = user.facebook_posts.recent.limit(from_each) +
-            user.gmail_messages.recent.limit(from_each) +
-            user.imgur_images.recent.limit(from_each) +
-            user.tweets.recent.limit(from_each)
+    # Tom doesn't, but Tom is known for being wrong.
+    sources = [user.facebook_posts.recent.limit(limit),
+               user.gmail_messages.recent.limit(limit),
+               user.imgur_images.recent.limit(limit),
+               user.tweets.recent.limit(limit)]
+    items = []
+    while items.length < limit && sources.any? { |s| s.length > 0 }
+      items += sources.map { |s| s.shift }.compact
+    end
     items.sort_by(&:published_at).reverse
   end
   
