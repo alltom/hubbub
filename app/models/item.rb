56726d60691a44ccb6914130f96d6cc4ae852df5
Rahul Rajagopalan <rahulrajago@gmail.com>
2012-05-13 22:48:49 -0400

    Tweaked the Item.recent algorithm, to try to show off more services.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,20 @@
 class Item
-  def self.recent limit=10
-    items = FacebookPost.recent.limit(limit) +
-            GmailMessage.recent.limit(limit) +
-            ImgurImage.recent.limit(limit) +
-            Tweet.recent.limit(limit)
-    items.sort_by(&:published_at).reverse.first(limit)
+  def self.recent limit=12
+    # Grabbing the limit most recent items regardless of table tends to produce
+    # an unbalanced feed of mostly Twitter posts and a little Facebook.
+    # Instead, grab a minimum from each source, because we want to demonstrate
+    # that we have live data from all four services.
+    #
+    # This also fixes the issue where we use up extra time by querying the
+    # database for limit * 4 items, then throw out limit * 3.
+    # 
+    # Sorting tends to bunch up services. Do you think this is a problem?
+    from_each = limit / 4
+    items = FacebookPost.recent.limit(from_each) +
+            GmailMessage.recent.limit(from_each) +
+            ImgurImage.recent.limit(from_each) +
+            Tweet.recent.limit(from_each)
+    items.sort_by(&:published_at).reverse
   end
   
   def self.model_for_source source
